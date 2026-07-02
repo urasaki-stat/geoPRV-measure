3a847196d5e38ed89c8f7ac1f499e4f0816d28e5
Wataru Urasaki <urasaki.stat@gmail.com>
2026-07-02 17:58:45 +0900

    Update geoPRV.R

--- geoPRV.R
+++ geoPRV.R
@@ -38,18 +38,3 @@ geoPRV <- function(dat, f, para){
        ) 
 }
 
-
-#####   Test Data   #####
-cannabis.dat <- matrix(c(204, 211, 357, 92, 6, 13, 44, 34, 1, 5, 38, 49), nrow = 4)
-dimnames(cannabis.dat) <- list(paste(c("At most once/month","Twice/month","Twice/week","More often")), 
-                               paste(c("Never", "Once or twice", "More often")))
-
-
-#####   Examples   #####
-f_lambda <- expression((x^(a+1) -x)/a)
-geoPRV(cannabis.dat, f_lambda, 1)
-
-f_lambda0 <- expression(x*log(x))
-geoPRV(cannabis.dat, f_lambda0, 0)
-
-
